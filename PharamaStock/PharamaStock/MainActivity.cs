b7fa5583d4979ef272725e74eea39e40d2809c2d
Julien Scemama <julien83600@outlook.com>
2018-09-21 14:34:51 +0200

    Plein de trucs

--- PharamaStock/PharamaStock/MainActivity.cs
+++ PharamaStock/PharamaStock/MainActivity.cs
@@ -11,6 +11,7 @@ using System.Text;
 
 namespace PharamaStock
 {
+//Theme = "@style/Theme.Design.Light.NoActionBar"
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
@@ -21,120 +22,139 @@ namespace PharamaStock
         
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(savedInstanceState);
-            //SetContentView(Resource.Layout.activity_main);
             
+            base.OnCreate(savedInstanceState);
+            ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+            Android.App.ActionBar.Tab tab = ActionBar.NewTab();
+            tab.SetIcon(Resource.Drawable.MANUEL);
+            tab.TabSelected += (sender, args) => {
+                // Do something when tab is selected
+            };
+            ActionBar.AddTab(tab);
+
+            tab = ActionBar.NewTab();
+            tab.SetIcon(Resource.Drawable.AUTO);
+            tab.TabSelected += (sender, args) =>
+            {
+                // Do something when tab is selected
+            };
+            ActionBar.AddTab(tab);
+            //SetContentView(Resource.Layout.activity_main);
+
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-            LinearLayout view = new LinearLayout(this)
-            {
-                Orientation = Orientation.Vertical
-            };
 
-            //affiche le numéro du patient
-            this.SetContentView(view);
-            TextView numPatient = new TextView(this)
-            {
-                Text = "Numéro du patient : "
-            };
-            view.AddView(numPatient);
-            EditText patient = new EditText(this)
-            {
+            
+            //LinearLayout view = new LinearLayout(this)
+            //{
+            //    Orientation = Orientation.Vertical
+            //};
 
-            };
-            patient.SetSingleLine(true);
-            view.AddView(patient);
-            this.SetContentView(view);
+            ////affiche le numéro du patient
+            //this.SetContentView(view);
+            //TextView numPatient = new TextView(this)
+            //{
+            //    Text = "Numéro du patient : "
+            //};
+            //view.AddView(numPatient);
+            //EditText patient = new EditText(this)
+            //{
 
-            //affiche le coe gef
-            TextView codeGef = new TextView(this)
-            {
-                Text = "Code GEF : "
-            };
-            view.AddView(codeGef);
-            EditText gef = new EditText(this)
-            {
+            //};
+            //patient.SetSingleLine(true);
+            //view.AddView(patient);
+            //this.SetContentView(view);
 
-            };
-            gef.SetSingleLine(true);
-            view.AddView(gef);
-            this.SetContentView(view);
+            ////affiche le coe gef
+            //TextView codeGef = new TextView(this)
+            //{
+            //    Text = "Code GEF : "
+            //};
+            //view.AddView(codeGef);
+            //EditText gef = new EditText(this)
+            //{
 
-            //affiche le numéro du lot
-            TextView numLot = new TextView(this)
-            {
-                Text = "Lot numéro : "
-            };
-            view.AddView(numLot); 
-             EditText lot = new EditText(this)
-            {
+            //};
+            //gef.SetSingleLine(true);
+            //view.AddView(gef);
+            //this.SetContentView(view);
 
-            };
-            patient.SetSingleLine(true);
-            view.AddView(lot);
-            this.SetContentView(view);
+            ////affiche le numéro du lot
+            //TextView numLot = new TextView(this)
+            //{
+            //    Text = "Lot numéro : "
+            //};
+            //view.AddView(numLot); 
+            // EditText lot = new EditText(this)
+            //{
 
-            //affiche la quantité délivrée
-            TextView quantiteDelivree = new TextView(this)
-            {
-                Text = "Quantité : "
-            };
-            view.AddView(quantiteDelivree); 
-             EditText quantite = new EditText(this)
-            {
+            //};
+            //patient.SetSingleLine(true);
+            //view.AddView(lot);
+            //this.SetContentView(view);
 
-            };
-            quantite.SetSingleLine(true);
-            view.AddView(quantite);
-            this.SetContentView(view);
+            ////affiche la quantité délivrée
+            //TextView quantiteDelivree = new TextView(this)
+            //{
+            //    Text = "Quantité : "
+            //};
+            //view.AddView(quantiteDelivree); 
+            // EditText quantite = new EditText(this)
+            //{
 
-            //affiche la date de délivrance à la date du jour
-            TextView date_display = new TextView(this)
-            {
-                Text = "date"
+            //};
+            //quantite.SetSingleLine(true);
+            //view.AddView(quantite);
+            //this.SetContentView(view);
 
-            };
-            view.AddView(date_display);
+            ////affiche la date de délivrance à la date du jour
+            //TextView date_display = new TextView(this)
+            //{
+            //    Text = "date"
 
-            TextView date = new TextView(this)
-            {
-                Text = DateTime.Now.ToLongDateString()
+            //};
+            //view.AddView(date_display);
 
-            };
-            view.AddView(date);
+            //TextView date = new TextView(this)
+            //{
+            //    Text = DateTime.Now.ToLongDateString()
 
-            DatePicker datepick = new DatePicker(this)
-            {
-                Visibility = Android.Views.ViewStates.Invisible
-            };
-            view.AddView(datepick);
+            //};
+            //view.AddView(date);
 
-            date.Click += (s, e) =>
-            {
-                datepick.Visibility = Android.Views.ViewStates.Visible;
-            };
+            //DatePicker datepick = new DatePicker(this)
+            //{
+            //    Visibility = Android.Views.ViewStates.Invisible
+            //};
+            //view.AddView(datepick);
 
-            datepick.DateChanged += (s, e) =>
-            {
-                date.Text = datepick.DateTime.ToLongDateString();
-                datepick.Visibility = Android.Views.ViewStates.Invisible;
-            };
+            //date.Click += (s, e) =>
+            //{
+            //    datepick.Visibility = Android.Views.ViewStates.Visible;
+            //};
 
-            //enregistre les données récoltées dans un fichier 
-            Button Enregistrer = new Button(this)
-            {
-                Text = "Enregistrer"
+            
+            //datepick.DateChanged += (s, e) =>
+            //{
+            //    date.Text = datepick.DateTime.ToLongDateString();
+            //    datepick.Visibility = Android.Views.ViewStates.Invisible;
+            //};
 
-            };
+            ////enregistre les données récoltées dans un fichier 
+            //Button Enregistrer = new Button(this)
+            //{
+            //    Text = "Enregistrer"
 
-            Enregistrer.Click += (s, e) =>
-            {
-                if(!string.IsNullOrEmpty(patient.Text) && !string.IsNullOrEmpty(gef.Text) && !string.IsNullOrEmpty(lot.Text) && !string.IsNullOrEmpty(quantite.Text) && !string.IsNullOrEmpty(date.Text))
-                CreateCSV(patient.Text, gef.Text, lot.Text, quantite.Text, date.Text);
-            };
+            //};
 
-            view.AddView(Enregistrer);
-            this.SetContentView(view);
+            //Enregistrer.Click += (s, e) =>
+            //{
+            //    if(!string.IsNullOrEmpty(patient.Text) && !string.IsNullOrEmpty(gef.Text) && !string.IsNullOrEmpty(lot.Text) && !string.IsNullOrEmpty(quantite.Text) && !string.IsNullOrEmpty(date.Text))
+            //    CreateCSV(patient.Text, gef.Text, lot.Text, quantite.Text, date.Text);
+            //};
+
+            //view.AddView(Enregistrer);
+           // this.SetContentView(view);
         }
 
         public void CreateCSV(string numpat, string codeGEF, string lotnum, string quant, string date)
