df88e5cb802efb86e240dff89caba1138ae404ef
Julien Scemama <julien83600@outlook.com>
2018-09-24 08:30:56 +0200

    fsdwghd

---
+++ PharamaStock/PharmaTab/MainActivity.cs
@@ -0,0 +1,168 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+using Android.Support.V7.App;
+using Android.Util;
+using Android.Widget;
+using PharmaTab.Fragments;
+using System;
+using System.IO;
+using System.Text;
+
+namespace PharmaTab
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true, LaunchMode = Android.Content.PM.LaunchMode.SingleTop, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        ViewPager pager;
+        TabsAdapter adapter;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.main);
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            if (toolbar != null)
+            {
+                SetSupportActionBar(toolbar);
+                SupportActionBar.SetDisplayHomeAsUpEnabled(false);
+                SupportActionBar.SetHomeButtonEnabled(false);
+
+            }
+
+            adapter = new TabsAdapter(this, SupportFragmentManager);
+            pager = FindViewById<ViewPager>(Resource.Id.pager);
+            var tabs = FindViewById<TabLayout>(Resource.Id.tabs);
+            pager.Adapter = adapter;
+            
+            tabs.SetupWithViewPager(pager);
+            pager.OffscreenPageLimit = 3;
+
+            Button savebt = FindViewById<Button>(Resource.Id.buttonenr);
+
+            savebt.Click += (s,e)  =>
+            {
+                //Création du fichier CSV
+                if (!string.IsNullOrEmpty(patient.Text) && !string.IsNullOrEmpty(gef.Text) && !string.IsNullOrEmpty(lot.Text) && !string.IsNullOrEmpty(quantite.Text) && !string.IsNullOrEmpty(date.Text))
+                    CreateCSV(patient.Text, gef.Text, lot.Text, quantite.Text, date.Text);
+
+                //Vide les champs d'entrée
+                quantite.Text = "";
+                lot.Text = "";
+                gef.Text = "";
+                patient.Text = "";
+            };
+        }
+
+        string directory = Android.OS.Environment.ExternalStorageDirectory + Java.IO.File.Separator + "Pharmastock";
+
+        //Méthode de création du fichier CSV
+        public void CreateCSV(string numpat, string codeGEF, string lotnum, string quant, string date)
+        {
+
+            //Création d'un dossier
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+                Toast.MakeText(Application.Context, "Dossier Pharmastock créé", ToastLength.Short).Show();
+
+            }
+            //Nom du fichier + Location
+            string fileName = directory + Java.IO.File.Separator + "Pharmastock_" + DateTime.Now.ToString("ddMMyyy") + ".csv";
+
+            //Ligne à ajouter lors de l'enregistrement. Reprend les entrées des champs EditText
+            var newline = string.Format("{0};{1};{2};{3};{4}", numpat, codeGEF, lotnum, quant, date);
+
+            //Si le fichier n'existe pas, créer les entêtes et aller à la ligne. 
+            if (!File.Exists(fileName))
+            {
+                string header = "Patient n° :" + ";" + "code GEF :" + ";" + "Lot n° :" + ";" + "Quantité :" + ";" + "Délivré le :";
+                File.WriteAllText(fileName, header, Encoding.UTF8);       // Création de la ligne + Encodage pour les caractères spéciaux
+                File.AppendAllText(fileName, System.Environment.NewLine); // Aller à la ligne
+            }
+            File.AppendAllText(fileName, newline + System.Environment.NewLine); // Ajout de la ligne contenant les champs
+            Toast.MakeText(Application.Context, "Données enregistrées", ToastLength.Short).Show();
+        }
+
+        public class DatePickerFragment : DialogFragment,
+                                 DatePickerDialog.IOnDateSetListener
+        {
+            // TAG can be any string of your choice.
+            public static readonly string TAG = "X:" + typeof(DatePickerFragment).Name.ToUpper();
+
+            // Initialize this value to prevent NullReferenceExceptions.
+            Action<DateTime> _dateSelectedHandler = delegate { };
+
+            public static DatePickerFragment NewInstance(Action<DateTime> onDateSelected)
+            {
+                DatePickerFragment frag = new DatePickerFragment();
+                frag._dateSelectedHandler = onDateSelected;
+                return frag;
+            }
+
+            public override Dialog OnCreateDialog(Bundle savedInstanceState)
+            {
+                DateTime currently = DateTime.Now;
+                DatePickerDialog dialog = new DatePickerDialog(Activity,
+                                                               this,
+                                                               currently.Year,
+                                                               currently.Month - 1,
+                                                               currently.Day);
+                return dialog;
+            }
+
+            public void OnDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+            {
+                // Note: monthOfYear is a value between 0 and 11, not 1 and 12!
+                DateTime selectedDate = new DateTime(year, monthOfYear + 1, dayOfMonth);
+                Log.Debug(TAG, selectedDate.ToLongDateString());
+                _dateSelectedHandler(selectedDate);
+            }
+        }
+
+
+        class TabsAdapter : FragmentStatePagerAdapter
+        {
+            string[] titles;
+
+            public override int Count
+            {
+                get
+                {
+                    return titles.Length;
+                }
+            }
+
+            public TabsAdapter(Context context, Android.Support.V4.App.FragmentManager fm) : base(fm)
+            {
+                titles = context.Resources.GetTextArray(Resource.Array.sections);
+            }
+
+            public override Java.Lang.ICharSequence GetPageTitleFormatted(int position)
+            {
+                return new Java.Lang.String(titles[position]);
+            }
+
+            public override Android.Support.V4.App.Fragment GetItem(int position)
+            {
+                switch (position)
+                {
+                    case 0:
+                        return Fragment1.NewInstance();
+                    case 1:
+                        return Fragment2.NewInstance();
+                }
+                return null;
+            }
+
+            public override int GetItemPosition(Java.Lang.Object frag)
+            {
+                return PositionNone;
+            }
+        }
+    }
+}
+
