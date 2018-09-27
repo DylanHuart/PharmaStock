0b651eccc83a48076b5d05c1a19325057726ff32
rudy joly <jolyrudy@msn.com>
2018-09-27 13:37:41 +0200

    Commentaires

--- PharamaStock/PharmaTab/Fragments/Fragment3.cs
+++ PharamaStock/PharmaTab/Fragments/Fragment3.cs
@@ -1,24 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Android.App;
 using Android.Content;
-using Android.Net;
 using Android.OS;
 using Android.Widget;
-using System;
 using Android.Support.V4.Content;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Util;
 using Android.Views;
-using Android.Widget;
+
 
 namespace PharmaTab.Fragments
 {
@@ -27,11 +16,10 @@ namespace PharmaTab.Fragments
 
         public override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(savedInstanceState);
-
-            // Create your fragment here
+            base.OnCreate(savedInstanceState);          
         }
 
+        //On instancie le fragment3 qui sert a afficher la page Historique
         public static Fragment3 NewInstance()
         {
             var frag3 = new Fragment3 { Arguments = new Bundle() };
@@ -43,41 +31,38 @@ namespace PharmaTab.Fragments
             var ignored = base.OnCreateView(inflater, container, savedInstanceState);
             var view = inflater.Inflate(Resource.Layout.fragment3, null);
 
-
-
-
-
+            //On créer la variable qui va donner la direction des fichiers dans le stockage interne de l'appareil
             string directory = Android.OS.Environment.ExternalStorageDirectory + Java.IO.File.Separator + "Pharmastock";
+
+            //On créer un tableau qui contient les chemins d'accès aux fichiers du dossier
             string[] fichiers = Directory.GetFiles(directory);
-            ArrayAdapter<string> fichiersAdapter;
-            fichiersAdapter = new ArrayAdapter<string>(this.Context, Android.Resource.Layout.SimpleListItemActivated1, fichiers);
 
-
+            //On créer une liste qui va afficher une ligne personnalisée pour chaque éléments du tableau
             List<string> fichierstxt = new List<string>();
             foreach (var item in fichiers)
                 fichierstxt.Add("Fichier du " + File.GetCreationTime(item));
 
+            //On met en place les "adapters" qui prennent en charge les éléments du tableau et de la liste
+            ArrayAdapter<string> fichiersAdapter = new ArrayAdapter<string>(this.Context, Android.Resource.Layout.SimpleListItemActivated1, fichiers);
             ArrayAdapter<string> fichierstxtAdapter = new ArrayAdapter<string>(this.Context, Android.Resource.Layout.SimpleListItemActivated1, fichierstxt);
 
-
-
-
+            //On appelle la liste view présente dans fragment3.axml
             ListView listehisto = view.FindViewById<ListView>(Resource.Id.listehisto);
-            listehisto.SetAdapter(fichierstxtAdapter);
 
-
+            //On associe l'adapter à la liste view
+            listehisto.Adapter = fichierstxtAdapter;
 
             // Valeur par défaut des items en false
             for (int i = 0; i < listehisto.Count; i++)
                 listehisto.SetItemChecked(i, false);
 
-
+            //On appelle les boutons ouvrir et supprimer de fragment3.axml et on déclare la méthode utilisée lors de l'évement Click
             Button btnOuvrir = view.FindViewById<Button>(Resource.Id.button2);
-            Button btnSuppr = view.FindViewById<Button>(Resource.Id.button1);
             btnOuvrir.Click += BtnOuvrir_Click;
+            Button btnSuppr = view.FindViewById<Button>(Resource.Id.button1);
             btnSuppr.Click += BtnSuppr_Click;
 
-
+            //On créer la méthode qui va ouvrir le fichier sélectionné avec Excel
             void BtnOuvrir_Click(object sender, EventArgs e)
             {
                 if (listehisto.CheckedItemCount == 1)
@@ -100,7 +85,7 @@ namespace PharmaTab.Fragments
                     alert.SetMessage("Veuillez sélectionner un fichier à ouvrir");
                     alert.SetButton("OK", (c, ev) =>
                     {
-                            // Ok button click task  
+                          
                     });
                     alert.Show();
                 }
@@ -109,54 +94,53 @@ namespace PharmaTab.Fragments
 
 
 
-
-        void BtnSuppr_Click(object sender, EventArgs e)
-        {
-            if (listehisto.CheckedItemCount > 0)
+            //On créer une méthode qui va supprimer le ou les fichiers sélectionnés
+            void BtnSuppr_Click(object sender, EventArgs e)
             {
-                var position = listehisto.CheckedItemPositions;
-                AlertDialog.Builder alert = new AlertDialog.Builder(this.Context);
-                alert.SetTitle("Suppression");
-                alert.SetMessage("Voulez-vous vraiment supprimer ?");
-                alert.SetPositiveButton("Supprimer", (senderAlert, args) =>
+                if (listehisto.CheckedItemCount > 0)
                 {
-                    for (int i = 0; i < listehisto.Count; i++)
+                    var position = listehisto.CheckedItemPositions;
+                    AlertDialog.Builder alert = new AlertDialog.Builder(this.Context);
+                    alert.SetTitle("Suppression");
+                    alert.SetMessage("Voulez-vous vraiment supprimer ?");
+                    alert.SetPositiveButton("Supprimer", (senderAlert, args) =>
                     {
-                        if (position.ValueAt(i) == true)
+                        for (int i = 0; i < listehisto.Count; i++)
                         {
-                            File.Delete(fichiers[i]);
+                            if (position.ValueAt(i) == true)
+                            {
+                                File.Delete(fichiers[i]);
+                            }
                         }
-                    }
-                    Intent historiqueActivity = new Intent(this.Context, typeof(Historique));
-                    StartActivity(historiqueActivity);
+                        Intent historiqueActivity = new Intent(this.Context, typeof(Historique));
+                        StartActivity(historiqueActivity);
 
-                    Toast.MakeText(this.Context, "Supprimé !", ToastLength.Short).Show();
+                        Toast.MakeText(this.Context, "Supprimé !", ToastLength.Short).Show();
 
-                });
-
-                alert.SetNegativeButton("Annuler", (senderAlert, args) =>
-                {
-                    Toast.MakeText(this.Context, "Annulé !", ToastLength.Short).Show();
-                });
-
-                Dialog dialog = alert.Create();
-                dialog.Show();
-            }
-            else
-            {
-                Android.App.AlertDialog.Builder dialog = new AlertDialog.Builder(this.Context);
-                AlertDialog alert = dialog.Create();
-                alert.SetTitle("Attention");
-                alert.SetMessage("Veuillez sélectionner un ou plusieurs fichiers à supprimer");
-                alert.SetButton("OK", (c, ev) =>
-                {
-                        // Ok button click task  
                     });
-                alert.Show();
-            }
-        }
 
-            return view;
+                    alert.SetNegativeButton("Annuler", (senderAlert, args) =>
+                    {
+                        Toast.MakeText(this.Context, "Annulé !", ToastLength.Short).Show();
+                    });
+
+                    Dialog dialog = alert.Create();
+                    dialog.Show();
+                }
+                else
+                {
+                    Android.App.AlertDialog.Builder dialog = new AlertDialog.Builder(this.Context);
+                    AlertDialog alert = dialog.Create();
+                    alert.SetTitle("Attention");
+                    alert.SetMessage("Veuillez sélectionner un ou plusieurs fichiers à supprimer");
+                    alert.SetButton("OK", (c, ev) =>
+                    {                        
+                    });
+                    alert.Show();
+                }
+            }
+
+           return view;
         }
-}
+    }
 }
